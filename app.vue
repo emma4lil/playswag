cc57995414c94d994445773cc38d46532ec44786
Regex <emma4lil@gmail.com>
2024-06-15 02:14:07 +0100

    initial

---
+++ app.vue
@@ -0,0 +1,29 @@
+<template>
+
+  <v-layout>
+    <v-navigation-drawer v-model="drawer" :rail="rail" permanent @click="rail = false">
+      <v-list-item prepend-avatar="https://randomuser.me/api/portraits/men/85.jpg" title="John Leider" nav>
+        <template v-slot:append>
+          <v-btn icon="mdi-chevron-left" variant="text" @click.stop="rail = !rail"></v-btn>
+        </template>
+      </v-list-item>
+
+      <v-divider></v-divider>
+
+      <v-list density="compact" nav>
+        <v-list-item prepend-icon="mdi-home-city" title="Home" value="home"></v-list-item>
+        <v-list-item prepend-icon="mdi-account" title="My Account" value="account"></v-list-item>
+        <v-list-item prepend-icon="mdi-account-group-outline" title="Users" value="users"></v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+    <v-main style="height: 250px">
+      <NuxtPage />
+    </v-main>
+  </v-layout>
+
+</template>
+
+<script lang="ts" setup>
+var drawer = ref(true)
+var rail = ref(true)
+</script>
---
+++ pages/workspace.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <h1>Default workspace</h1>
+    <v-btn  color="success">Load data</v-btn>
+  </div>
+</template>
+
+<script lang="ts" setup>
+
+</script>
+
+<style></style>
